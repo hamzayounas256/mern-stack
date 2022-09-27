78534291b4cef186c48992fce7586ce8a3410c12
Mughal005 <deadbody009@gmail.com>
2022-09-27 11:34:04 +0500

    26

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 window.onload = function()
 {
+    console.log("loading...");
+    var inp1 = document.getElementById("myinp1");
+    var inp2 = document.getElementById("myinp2");
+    // var res = document.getElementById("res");
     var btn = document.getElementById("mybtn");
-    btn.onclick =  function(){
-        console.log("Hamza younas");
-    }
-    btn.onmouseleave = function(){
-        console.log("Muhammad younas");
+    btn = Number(inp1) + Number(inp2);
+    // res.innerHTML = btn;
+    btn.onclick = function(){
+        // res = btn.value;
+        console.log("The result is the ");
+        console.log(btn);
     }
 }
